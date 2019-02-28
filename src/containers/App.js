f5a3964442336fa7ecc1aba2b5f1ef7674053d0f
Nick Santoro <santoro.nick@gmail.com>
2019-02-28 10:29:34 -0800

    styling the navbar component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { configureStore } from "../store";
 import { BrowserRouter as Router } from "react-router-dom";
+import Navbar from "./Navbar"
 
 const store = configureStore();
 
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
-        Hello World
+      <div className="onboarding">
+        <Navbar />
       </div>
     </Router>
   </Provider>
 )
 
 export default App;
+
