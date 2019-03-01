0569f08234dc2d0251725690f0d44b1bfa099e7e
Nick Santoro <santoro.nick@gmail.com>
2019-03-01 12:46:25 -0800

    creating authform continued

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -10,33 +10,75 @@ class AuthForm extends Component {
       profileImageUrl: ""
     };
   }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const authType = this.props.signUp ? "signup" : "signin";
+    this.props.onAuth(authType, this.state).then(() => {
+      console.log("Logged in Success");
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
   render() {
     const { email, username, password, profileImageUrl } = this.state
-    const { heading, buttonText } = this.props
+    const { heading, buttonText, signUp } = this.props
     return (
       <div>
-        <div className="row justify-content-md-center text-center"></div>
-        <div className="col-md-6">
-          <form onSubmit={this.handleSubmit}>
-            <h2>{heading}</h2>
-            <label htmlFor="email">Email:</label>
-            <input
-              className="form-control"
-              id="email"
-              name="email"
-              onChange={this.handleChange}
-              value={email}
-              type="text"
-            />
-            <label htmlFor="password">Password:</label>
-            <input
-              className="form-control"
-              id="password"
-              name="password"
-              onChange={this.handleChange}
-              type="password"
-            />
-          </form>
+        <div className="row justify-content-md-center text-center">
+          <div className="col-md-6">
+            <form onSubmit={this.handleSubmit}>
+              <h2>{heading}</h2>
+              <label htmlFor="email">Email:</label>
+              <input
+                className="form-control"
+                id="email"
+                name="email"
+                onChange={this.handleChange}
+                value={email}
+                type="text"
+              />
+              <label htmlFor="password">Password:</label>
+              <input
+                className="form-control"
+                id="password"
+                name="password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              {signUp && (
+                <div>
+                  <label htmlFor="username">Username:</label>
+                  <input
+                    className="form-control"
+                    id="username"
+                    name="username"
+                    onChange={this.handleChange}
+                    value={username}
+                    type="text"
+                  />
+                  <label htmlFor="image-url">Image URL:</label>
+                  <input
+                    className="form-control"
+                    id="image-url"
+                    name="profileImageUrl"
+                    onChange={this.handleChange}
+                    type="text"
+                    value={profileImageUrl}
+                  />
+
+                </div>
+              )}
+              <button type="submit" className="btn btn-primary btn-block btn-lrg">
+                {buttonText}
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     )
--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,20 +3,28 @@ import { Switch, Route, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import Homepage from "../components/Homepage";
 import AuthForm from "../components/AuthForm";
+import { authUser } from "../store/actions/auth";
 
 const Main = props => {
+  const { authUser } = props;
   return (
     <div className="container">
       <Switch>
         <Route exact path="/" render={props => <Homepage {...props} />} />
         <Route exact path="/signin" render={props => {
           return (
-            <AuthForm buttonText="Log in" heading="Welcome Back." {...props} />
+            <AuthForm onAuth={authUser} buttonText="Log in" heading="Welcome Back." {...props} />
           )
         }} />
         <Route exact path="/signup" render={props => {
           return (
-            <AuthForm buttonText="Sign me up" heading="Join Warbler Today!" {...props} />
+            <AuthForm
+              onAuth={authUser}
+              signUp
+              buttonText="Sign me up"
+              heading="Join Warbler Today!"
+              {...props}
+            />
           )
         }} />
       </Switch>
@@ -30,4 +38,4 @@ function mapStateToProps(state) {
   };
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, { authUser })(Main));
